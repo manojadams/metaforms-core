29633f44489b562a7bb015ebd9ef1f994e959da2
Manoj <manoj.adams@gmail.com>
2023-08-24 20:24:33 +0530

    tcs util

---
+++ src/__tests__/utils/MetaformUtil.test..tsx
@@ -0,0 +1,7 @@
+import MetaformUtil from "../../utils/MetaformUtil";
+
+describe("FormUtill tests", () => {
+    it("FormUtill is truthy", () => {
+        expect(MetaformUtil).toBeTruthy();
+    });
+});
