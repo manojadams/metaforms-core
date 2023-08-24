7c24ce4c1863c3d3319120ce5207005653335485
Manoj <manoj.adams@gmail.com>
2023-08-24 13:44:43 +0530

    test updates

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,31 +1,8 @@
 import React from "react";
-import { render } from "@testing-library/react";
 import FormRenderer from "../MetaformRenderer";
-import * as schema1 from "./data/schema_1.json";
 
 describe("FormRenderer", () => {
     it("is truthy", () => {
         expect(FormRenderer).toBeTruthy();
-    });
-    it("Min. schema test", () => {
-        const output = render(
-            <FormRenderer
-                schema={{ fields: [] }}
-                onSubmit={() => {
-                    console.log("Schema submit");
-                }}
-            />
-        );
-        expect(output.getByText("")).toBeTruthy();
-    });
-    it("Schema 1 test", () => {
-        render(
-            <FormRenderer
-                schema={schema1}
-                onSubmit={() => {
-                    console.log("Schema1 submit");
-                }}
-            />
-        );
     });
 });
