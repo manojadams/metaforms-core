6d512dcba32abcd2bbe037f43dbb9c1fb42dd375
Manoj Baruah <manoj.adams@gmail.com>
2026-05-18 01:01:47 +0530

    feature-111: fix build issue

--- src/form/form-stepper/index.tsx
+++ src/form/form-stepper/index.tsx
@@ -3,7 +3,9 @@ import FormImplsContext from "../form-impl-context";
 import { IField } from "../../constants/model-interfaces";
 
 function FormGroupStepper(props: { fields: Array<IField>; theme?: string }) {
-    const { IBaseFormStepper } = useContext(FormImplsContext);
+    const formImplContext = useContext(FormImplsContext);
+    if (!formImplContext) return null;
+    const { IBaseFormStepper } = formImplContext;
     return <IBaseFormStepper {...props} theme={props.theme || ""} />;
 }
 
--- src/form/form-wizard/index.tsx
+++ src/form/form-wizard/index.tsx
@@ -3,7 +3,9 @@ import FormImplsContext from "../form-impl-context";
 import { IField } from "../../constants/model-interfaces";
 
 function FormGroupWizard(props: { fields: Array<IField>; theme?: string }) {
-    const { IBaseFormWizard } = useContext(FormImplsContext);
+    const formImplContext = useContext(FormImplsContext);
+    if (!formImplContext) return null;
+    const { IBaseFormWizard } = formImplContext;
     return <IBaseFormWizard {...props} theme={props.theme || ""} />;
 }
 
